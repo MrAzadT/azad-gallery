1d092d5f923575648c0b0a4d366094c8d8e572ab
azadtamal <mrazadt@gmail.com>
2021-10-09 00:11:13 +0600

    function

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYpwTGmlsrCExjqogv_yxXeg64CXqGA64",
+  authDomain: "azad-photo-gallery.firebaseapp.com",
+  projectId: "azad-photo-gallery",
+  storageBucket: "azad-photo-gallery.appspot.com",
+  messagingSenderId: "24062064500",
+  appId: "1:24062064500:web:3c6203781c7c1481711e21",
+};
+
+
+const app = initializeApp(firebaseConfig);
