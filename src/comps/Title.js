02987d0e278cbec210894f49ae2d920f838e02f7
azadtamal <mrazadt@gmail.com>
2021-10-08 16:13:50 +0600

    azad

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -9,8 +9,8 @@ const Title = () => {
         src="https://res.cloudinary.com/abidazad/image/upload/v1632643636/Solid_uo4lnz.svg"
         alt=""
       />
+      <h1>Azad Gallery</h1>
       <h2>Upload Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   );
 };
