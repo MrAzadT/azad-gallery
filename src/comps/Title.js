3137639852a0e1b1e28e9574626f6856465956ce
azadtamal <mrazadt@gmail.com>
2021-10-08 16:06:52 +0600

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <img
+        className="img"
+        src="https://res.cloudinary.com/abidazad/image/upload/v1632643636/Solid_uo4lnz.svg"
+        alt=""
+      />
+      <h2>Upload Your Pictures</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+};
+
+export default Title;
